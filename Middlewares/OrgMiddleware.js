fec3ac7662388763580eb3715ebc2c986268b05e
Hong Van <hongvanpc10@gmail.com>
2024-05-19 12:15:43 +0700

    add account field to return data of getinfo

--- Middlewares/OrgMiddleware.js
+++ Middlewares/OrgMiddleware.js
@@ -1,18 +1,22 @@
-import AuthError from "../Errors/AuthError.js";
-import Organization from "../Models/Organization.js";
-import { ORGANIZATION } from "../Utils/Constraints.js";
+import AuthError from '../Errors/AuthError.js'
+import Organization from '../Models/Organization.js'
+import { ORGANIZATION } from '../Utils/Constraints.js'
 
-const OrgMiddleware=async(req,res,next)=>{
-          try{
-                    var account=req.account;
-                    if(account.role!=ORGANIZATION) throw new AuthError("This endpoint is only for organizations");
-                    var org=await Organization.findOne({account:account._id});
-                    if(!org) throw new AuthError("Organization with accountId "+account._id+" does not exist");
-                    req.org=org;
-                    next();
-          }
-          catch(error){
-                    next(error);
-          }
+const OrgMiddleware = async (req, res, next) => {
+	try {
+		var account = req.account
+		if (account.role != ORGANIZATION)
+			throw new AuthError('This endpoint is only for organizations')
+		var org = await Organization.findOne({ account: account._id })
+		if (!org)
+			throw new AuthError(
+				'Organization with accountId ' + account._id + ' does not exist'
+			)
+		req.org = org
+		req.org.account = account
+		next()
+	} catch (error) {
+		next(error)
+	}
 }
-export default OrgMiddleware;
+export default OrgMiddleware
--- Middlewares/StudentMiddleware.js
+++ Middlewares/StudentMiddleware.js
@@ -1,18 +1,22 @@
-import AuthError from "../Errors/AuthError.js";
-import Student from "../Models/Student.js";
-import { STUDENT } from "../Utils/Constraints.js";
+import AuthError from '../Errors/AuthError.js'
+import Student from '../Models/Student.js'
+import { STUDENT } from '../Utils/Constraints.js'
 
-const StudentMiddleware=async(req,res,next)=>{
-          try{
-                    var account=req.account;
-                    if(account.role!=STUDENT) throw new AuthError("This endpoint is only for students");
-                    var student=await Student.findOne({account:account._id});
-                    if(!student) throw new AuthError("Student with accountId "+account._id+" does not exist");
-                    req.student=student;
-                    next();
-          }
-          catch(error){
-                    next(error);
-          }
+const StudentMiddleware = async (req, res, next) => {
+	try {
+		var account = req.account
+		if (account.role != STUDENT)
+			throw new AuthError('This endpoint is only for students')
+		var student = await Student.findOne({ account: account._id })
+		if (!student)
+			throw new AuthError(
+				'Student with accountId ' + account._id + ' does not exist'
+			)
+		req.student = student
+		req.student.account = account
+		next()
+	} catch (error) {
+		next(error)
+	}
 }
-export default StudentMiddleware;
+export default StudentMiddleware
