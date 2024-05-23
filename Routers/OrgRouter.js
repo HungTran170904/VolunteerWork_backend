473599f6a1b7035141b0aee541f16c92a668ba3d
HungTran170904 <tienhung17092004@gmail.com>
2024-05-23 16:02:17 +0700

    change db url

--- Routers/OrgRouter.js
+++ Routers/OrgRouter.js
@@ -10,11 +10,22 @@ import AuthMiddleware from "../Middlewares/AuthMiddleware.js";
 
 const OrgRouter=express.Router();
 
+// get info of logined organization
 OrgRouter.get("/loginedInfo",OrgMiddleware,OrgController.getLoginedInfo);
+
+// get general info of an organization
 OrgRouter.get("/organizationInfo",OrgController.getOrganizationInfo);
+
+// upload avatar
 OrgRouter.post("/uploadAvatar",OrgMiddleware,UploadMiddleware,OrgController.uploadAvatar);
+
+// admin verify organization
 OrgRouter.get("/verifyOrganization",AdminMiddleware,OrgController.verifyOrganization);
+
+// update organization
 OrgRouter.post("/updateOrganization",OrgMiddleware, OrgController.updateOrganization);
+
+// get organization with pagination
 OrgRouter.post("/organizations", OrgController.getOrganizations);
 
 export default OrgRouter;
